ed7f9f5bbe73227a1bcac4d493657a109ae7437a
dely22 <almktaryh@gmail.com>
2022-09-02 16:02:34 -0800

    Fix - Chart Json Url

--- js/chart.js
+++ js/chart.js
@@ -1,18 +1,20 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "http://127.0.0.1:5501/js/jsonData.json";
+var url = "/js/jsonData.json";
+// var url = "https://drive.google.com/file/d/1ctwfT0AEvL22MgQen1XbQXl5PJWBfKmE/view?usp=sharing";
+
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var data = JSON.parse(this.responseText);
-        //console.log(data)
+        console.log(data)
         date = data.date_Orders.map(function (elem) {
             return elem.date;
         })
         Orders = data.date_Orders.map(function (elem) {
             return elem.Orders;
         })
-        //console.log(Orders)
+        console.log(Orders)
 
         const ctx = document.getElementById('canvas').getContext('2d');
         const myChart = new Chart(ctx, {
